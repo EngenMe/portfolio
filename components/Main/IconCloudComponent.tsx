26dcf46eeeaf31370119efe93e5707fa3f3aee6e
EngenMe <mohamdfarouk727@gmail.com>
2024-12-20 10:31:15 +0000

    Make icon cloud responsive.

--- components/Main/IconCloudComponent.tsx
+++ components/Main/IconCloudComponent.tsx
@@ -35,7 +35,7 @@ const slugs = [
 
 const IconCloudComponent = () => {
   return (
-    <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg bg-background px-20 pb-20 pt-8 ">
+    <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg bg-background px-4 md:px-10 lg:px-20 pb-20 pt-8 ">
       <IconCloud iconSlugs={slugs} />
     </div>
   );
