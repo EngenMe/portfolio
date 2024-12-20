2422e4f7f41dbb4f5bc6cbed17848c3755b85818
EngenMe <mohamdfarouk727@gmail.com>
2024-12-20 18:45:17 +0000

    Remove image from next config.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["avatar.vercel.sh"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
