e7028a40cb438a7fcd049b4fd55d84935192fb41
EngenMe <mohamdfarouk727@gmail.com>
2024-12-22 17:37:13 +0000

    Change cv.

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = () => {
     <h1
       className={cn(
         suse.className,
-        "text-4xl lg:text-5xl font-extrabold bg-gradient-to-r from-[#6765F1] to-[#EA51A0] bg-clip-text text-transparent cursor-pointer hover:scale-125 animate-blur-in transition-transform duration-300"
+        "text-4xl lg:text-5xl font-extrabold bg-gradient-to-r from-[#6765F1] to-[#EA51A0] bg-clip-text text-transparent cursor-pointer lg:hover:scale-125 animate-blur-in transition-transform duration-300"
       )}
     >
       <Link href="/">EngenMe</Link>
--- data/contacts.data.ts
+++ data/contacts.data.ts
@@ -2,7 +2,7 @@ import { Contact } from "@/interfaces/Contact.interface";
 import { Mail, MapPin, Phone } from "lucide-react";
 
 export const contacts: Contact[] = [
-  { icon: Phone, label: "Phone", contact: "089 948 5281" },
+  { icon: Phone, label: "Phone", contact: "+353 89 948 5281" },
   { icon: Mail, label: "Email", contact: "mohamdfarouk727@gmail.com" },
   {
     icon: MapPin,
