ce13244c2cd64580cb66752d5bda2e1a32926663
EngenMe <mohamdfarouk727@gmail.com>
2024-12-23 11:53:28 +0000

    Change social media data.

--- data/socialMedias.data.ts
+++ data/socialMedias.data.ts
@@ -1,31 +1,24 @@
 import { ISocialMediaItem } from "@/interfaces/ISocialMediaItem.interface";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { SiLeetcode } from "react-icons/si";
 
 export const socialMedias: ISocialMediaItem[] = [
   {
-    icon: FaGithub,
-    url: "https://github.com/EngenMe",
-    inLightModeColor: "#24292F",
-    inDarkModeColor: "#F5F5F5",
-  },
-  {
     icon: FaLinkedin,
     url: "https://www.linkedin.com/in/engenme/",
-    inLightModeColor: "#0077B5",
-    inDarkModeColor: "#00A0DC",
+    inLightModeColor: "#0A66C2",
+    inDarkModeColor: "#0A66C2",
+  },
+  {
+    icon: FaInstagram,
+    url: "https://www.instagram.com/engenme_mohamed/profilecard/?igsh=MTUyZjlxaGM4dW84dA==",
+    inLightModeColor: "#E4405F",
+    inDarkModeColor: "#E4405F",
   },
   {
     icon: FaSquareXTwitter,
     url: "https://x.com/Mohamed06058360",
     inLightModeColor: "#000000",
     inDarkModeColor: "#E7E9EA",
-  },
-  {
-    icon: SiLeetcode,
-    url: "https://leetcode.com/u/0BDoSenkrc/",
-    inLightModeColor: "#FFD923",
-    inDarkModeColor: "#FFD983",
   },
 ];
