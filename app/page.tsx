e4b0df3394273d739c0c41f28b14016ff7a6da75
EngenMe <mohamdfarouk727@gmail.com>
2024-12-24 20:24:29 +0000

    Add h1 header.

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import BlurIn from "@/components/ui/blur-in";
 const Main = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <BlurIn
-        word="Transforming Your Vision Into Digital Reality"
-        className="text-4xl font-bold text-primary dark:text-white"
-      />
+      <h1>
+        <BlurIn
+          word="Transforming Your Vision Into Digital Reality"
+          className="text-4xl font-bold text-primary dark:text-white"
+        />
+      </h1>
       <GlobeComponent />
     </div>
   );
--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -8,14 +8,14 @@ const suse = SUSE({
 
 const Logo = () => {
   return (
-    <h1
+    <header
       className={cn(
         suse.className,
         "text-4xl lg:text-5xl font-extrabold bg-gradient-to-r from-[#6765F1] to-[#EA51A0] bg-clip-text text-transparent cursor-pointer lg:hover:scale-125 animate-blur-in transition-transform duration-300"
       )}
     >
       <Link href="/">EngenMe</Link>
-    </h1>
+    </header>
   );
 };
 
