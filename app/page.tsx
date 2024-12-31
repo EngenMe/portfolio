e9029527941fce92054b2312a7693f5e4faac744
EngenMe <mohamdfarouk727@gmail.com>
2024-12-31 18:59:40 +0000

    Update landing pages.

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import BlurIn from "@/components/ui/blur-in";
 const Main = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1>
-        <BlurIn
-          word="Transforming Your Vision Into Digital Reality"
-          className="text-4xl font-bold text-primary dark:text-white"
-        />
-      </h1>
+      <BlurIn
+        word="Transforming Your Vision Into Digital Reality"
+        className="text-4xl font-bold text-primary dark:text-white"
+      />
       <GlobeComponent />
     </div>
   );
